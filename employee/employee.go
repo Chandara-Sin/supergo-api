10c45c6ad513b93bd0eccbb9c73efff63dc71e6b
Chandara-Sin <cdsin68@gmail.com>
2022-08-04 21:15:50 +0700

    refactor: change variable name

--- employee/employee.go
+++ employee/employee.go
@@ -3,7 +3,7 @@ package employee
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Employee struct {
-	Id    primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	ID    primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	Name  string             `bson:"name" json:"name"`
 	Email string             `bson:"email" json:"email"`
 }
