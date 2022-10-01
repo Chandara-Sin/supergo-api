c35daa7ee3445cb8512eb4fb45d522094af75e2c
Chandara-Sin <cdsin68@gmail.com>
2022-10-01 20:02:20 +0700

    feat: add create user api

---
+++ domain/user/create_user_handler.go
@@ -0,0 +1,44 @@
+package user
+
+import (
+	"Chandara-Sin/supergo-api/logger"
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type createUserFunc func(context.Context, User) error
+
+func (fn createUserFunc) CreateUser(ctx context.Context, reqUser User) error {
+	return fn(ctx, reqUser)
+}
+
+func CreateUserHandler(svc createUserFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var reqEmp User
+		log := logger.Unwrap(c)
+
+		if err := c.Bind(&reqEmp); err != nil {
+			log.Error(err.Error())
+			return c.JSON(http.StatusBadRequest, echo.Map{
+				"error": err.Error(),
+			})
+		}
+
+		reqEmp.CreatedAt = time.Now()
+		reqEmp.UpdatedAt = time.Now()
+
+		err := svc.CreateUser(c.Request().Context(), reqEmp)
+		if err != nil {
+			log.Error(err.Error())
+			return c.JSON(http.StatusInternalServerError, echo.Map{
+				"error": err.Error(),
+			})
+		}
+		return c.JSON(http.StatusOK, echo.Map{
+			"message": "status ok",
+		})
+	}
+}
