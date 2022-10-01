5978ae7fc98b3ea455db01eb3771702caac9c4fe
Chandara-Sin <cdsin68@gmail.com>
2022-10-01 20:03:30 +0700

    feat: add update user api

---
+++ domain/user/update_user_repository.go
@@ -0,0 +1,38 @@
+package user
+
+import (
+	"context"
+	"errors"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func Update(db *mongo.Database) func(context.Context, User) error {
+	return func(ctx context.Context, usr User) error {
+		collection := getUserCollection(db)
+
+		objID, err := primitive.ObjectIDFromHex(usr.ID.Hex())
+		if err != nil {
+			return err
+		}
+
+		aByte, err := bson.Marshal(usr)
+		if err != nil {
+			return err
+		}
+
+		var updatedEmp bson.M
+		err = bson.Unmarshal(aByte, &updatedEmp)
+		if err != nil {
+			return err
+		}
+
+		rs, err := collection.UpdateByID(ctx, objID, bson.D{{Key: "$set", Value: updatedEmp}})
+		if rs.ModifiedCount == 0 {
+			return errors.New("can not update")
+		}
+		return err
+	}
+}
