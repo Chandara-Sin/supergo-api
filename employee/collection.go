9a8b513bb32b7b234a8b4feb6b90bd9ac82cb752
Chandara-Sin <cdsin68@gmail.com>
2022-10-01 18:19:35 +0700

    refactor: add auto increment to employ struct

--- employee/collection.go
+++ employee/collection.go
@@ -5,3 +5,7 @@ import "go.mongodb.org/mongo-driver/mongo"
 func getEmployeeCollection(db *mongo.Database) *mongo.Collection {
 	return db.Collection("employees")
 }
+
+func getCounterCollection(db *mongo.Database) *mongo.Collection {
+	return db.Collection("counter")
+}
