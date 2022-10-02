a67216f125392fe54f58a2dc84d4ed369b4b4d3d
Chandara-Sin <cdsin68@gmail.com>
2022-10-02 17:43:00 +0700

    refactor: add auto increment user id

--- domain/user/user.go
+++ domain/user/user.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"fmt"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -14,16 +15,20 @@ const (
 	NotApplicable Gender = "n/a"
 )
 
+func (usr *User) SetUserID(usrID int64) {
+	usr.UserId = "US" + fmt.Sprintf("%05d", usrID)
+}
+
 type User struct {
-	ID primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	// UserId    int                `bson:"user_id,omitempty" json:"user_id,omitempty"`
-	FirstName string    `bson:"first_name" json:"first_name"`
-	LastName  string    `bson:"last_name" json:"last_name"`
-	Gender    Gender    `bson:"gender" json:"gender"`
-	Email     string    `bson:"email" json:"email"`
-	Address   Address   `bson:"address" json:"address"`
-	CreatedAt time.Time `bson:"created_at,omitempty" json:"created_at,omitempty"`
-	UpdatedAt time.Time `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserId    string             `bson:"user_id,omitempty" json:"user_id,omitempty"`
+	FirstName string             `bson:"first_name" json:"first_name"`
+	LastName  string             `bson:"last_name" json:"last_name"`
+	Gender    Gender             `bson:"gender" json:"gender"`
+	Email     string             `bson:"email" json:"email"`
+	Address   Address            `bson:"address" json:"address"`
+	CreatedAt time.Time          `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	UpdatedAt time.Time          `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
 }
 
 type Address struct {
