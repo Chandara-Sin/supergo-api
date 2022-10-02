e19b0b346fb889e6ed15e533668c5d8b9e850ca9
Chandara-Sin <cdsin68@gmail.com>
2022-10-02 17:43:00 +0700

    refactor: add auto increment user id

--- domain/user/create_user_repository.go
+++ domain/user/create_user_repository.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"Chandara-Sin/supergo-api/domain/counter"
 	"context"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -9,7 +10,14 @@ import (
 func Create(db *mongo.Database) func(context.Context, User) error {
 	return func(ctx context.Context, usr User) error {
 		collection := getUserCollection(db)
-		_, err := collection.InsertOne(ctx, usr)
+
+		usrID, err := counter.CounterForNextSequence(db, ctx, "users")
+		if err != nil {
+			return err
+		}
+
+		usr.SetUserID(usrID)
+		_, err = collection.InsertOne(ctx, usr)
 		return err
 	}
 }
